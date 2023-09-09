baa3cbe14faebea86fd001e44ce8598da5d0de89
FD70000000 <halfl.ez.ser@mail.ru>
2023-09-09 15:02:47 +0300

    first commit

---
+++ src/components/FreakingBrick.vue
@@ -0,0 +1,22 @@
+<template>
+    <img 
+        class="freaking_brick"
+        src = "./images/brick.png"
+        alt = "freaking brick"
+    
+        >
+
+  </template>
+  
+  <style scoped>
+    .freaking_brick {
+      /* background-color: black; */
+      width: 320px;
+      height: 150px;
+
+      margin: 2px;
+      margin-bottom: 2px;
+      display: inline-block;
+    }
+  </style>
+  
---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,10 @@
+<script setup>
+import FREAKING_BRICK from './FreakingBrick.vue'
+</script>
+
+<template>
+  <FREAKING_BRICK />
+  <FREAKING_BRICK />
+  <FREAKING_BRICK />
+
+</template>
